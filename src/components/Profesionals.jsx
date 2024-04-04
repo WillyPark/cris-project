492b770feebf5f8948b8a2bcc33997c0ca83679d
Luis <garuluisnaruto@hotmail.com>
2024-04-04 11:29:36 -0600

    form added in Profesionals section

--- src/components/Profesionals.jsx
+++ src/components/Profesionals.jsx
@@ -2,7 +2,43 @@ export const Profesionals = () => {
     return (
         <main className="fadeOut">
             <div className="profesionals-contenedor">
-                <p>You are already a CRA wanting to be better professional by collaborating with us?</p>
+                <p>You are already a <strong>CRA</strong> wanting <strong>to be better profesional</strong> by collaborating with us?</p>
+
+                <form action="" className="profesionals-formulario" method="POST">
+                    <div className="campo">
+                        <label htmlFor="company">Company:</label>
+                        <input type="text" placeholder="Your company" id="company" name="company" />
+                    </div>
+
+                    <div className="campo">
+                        <label htmlFor="email">Email address:</label>
+                        <input type="email" placeholder="Your email" id="email" name="email" />
+                    </div>
+
+                    <div className="campo">
+                        <label htmlFor="emailConfirm">Your email again as verification:</label>
+                        <input type="email" placeholder="Your email again" id="emailConfirm" />
+                    </div>
+
+                    <div className="campo">
+                        <label htmlFor="phone">Phone:</label>
+                        <input type="text" placeholder="Your phone number" id="phone" name="phone" />
+                    </div>
+
+                    <div className="campo">
+                        <label htmlFor="date">Time expectation to onborarding in you study: </label>
+                        <input type="date" placeholder="Select a day" id="date" name="date" />
+                    </div>
+
+                    <div className="campo">
+                        <label htmlFor="comment">Comment:</label>
+                        <input type="text" placeholder="Any comments" id="comment" name="comment" />
+                    </div>
+
+                    <div className="btn-submit">
+                        <button type="submit" disabled>Enviar</button>
+                    </div>
+                </form>
             </div>
         </main>
     )
--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
         <footer className="footer">
             <div className="contenedor">
                 <div className="footer__linea"></div>
-                <p>CRAs back-up &copy; 2023</p>
+                <p>CRAs back-up &copy; 2024</p>
             </div>
         </footer>
     )
