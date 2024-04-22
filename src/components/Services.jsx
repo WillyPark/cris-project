7464533da2c4919639d3acd277b721ff15c2948c
Luis <garuluisnaruto@hotmail.com>
2024-04-21 22:48:24 -0600

    services added and almost finished monitoring

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,14 +1,48 @@
 import { useContext } from "react";
 import { LanguageContext } from "../context/LanguageContext";
+import { Words } from "./Words";
+import { NavLink } from "react-router-dom";
 
 export const Services = () => {
     const { language } = useContext(LanguageContext);
 
     return (
-        <div className="contenedor-contenido-principal">
-            <main className="contenedor fadeOut">
+        <>
+            <div className="fondo-servicios">
+                <div className="fadeOut">
+                    <Words />
+                </div>
+            </div>
+
+            <main className="servicios contenedor">
                 <h1>{language.services.title}</h1>
+
+                <div className="servicios-contenedor">
+                    <div className="servicios__servicio">
+                        <h2>{language.services.serviceOne.title[0]} <span className="monitoring__experiencia--coloreado">{language.services.serviceOne.title[1]}</span></h2>
+
+                        <p>{language.services.serviceOne.text}</p>
+                    </div>
+
+                    <div className="servicios__servicio">
+                        <h2>{language.services.serviceTwo.title[0]} <span className="monitoring__experiencia--coloreado">{language.services.serviceTwo.title[1]}</span></h2>
+
+                        <p>{language.services.serviceTwo.text}</p>
+                    </div>
+
+                    <div className="servicios__servicio">
+                        <h2>{language.services.serviceTree.title[0]} <span className="monitoring__experiencia--coloreado">{language.services.serviceTree.title[1]}</span></h2>
+
+                        <p>{language.services.serviceTree.text[0]}</p>
+                        <p>{language.services.serviceTree.text[1]}</p>
+                        <p>{language.services.serviceTree.text[2]}</p>
+                    </div>
+                </div>
+
+                <NavLink className="servicios__boton" to="/contact">
+                    {language.services.button}
+                </NavLink>
             </main>
-        </div>
+        </>
     )
 }
--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -1,7 +1,48 @@
+import { useContext } from "react";
+import { LanguageContext } from "../context/LanguageContext";
+
 export const Words = () => {
+    const { language } = useContext(LanguageContext);
+
     return (
-        <section>
-            <h1>Site Initiation</h1>
+        <section className="palabras">
+            <div className="fila">
+                <p className="palabras-uno">{language.words.uno}</p>
+                <p className="palabras-dos">{language.words.dos}</p>
+            </div>
+
+            <div className="fila">
+                <p className="palabras-tres">{language.words.tres}</p>
+                <p className="palabras-cuatro">{language.words.cuatro}</p>
+                <p className="palabras-cinco">{language.words.cinco}</p>
+            </div>
+
+            <div className="fila">
+                <p className="palabras-seis">{language.words.seis}</p>
+                <p className="palabras-siete">{language.words.siete}</p>
+                <p className="palabras-ocho">{language.words.ocho}</p>
+            </div>
+
+            <div className="fila">
+                <p className="palabras-nueve">{language.words.nueve}</p>
+                <p className="palabras-diez">{language.words.diez}</p>
+                <p className="palabras-once">{language.words.once}</p>
+                <p className="palabras-doce">{language.words.doce}</p>
+            </div>
+
+            <div className="fila">
+                <p className="palabras-trece">{language.words.trece}</p>
+                <p className="palabras-catorce">{language.words.catorce}</p>
+                <p className="palabras-quince">{language.words.quince}</p>
+                <p className="palabras-dieciseis">{language.words.dieciseis}</p>
+            </div>
+
+            <div className="fila">
+                <p className="palabras-diecisiete">{language.words.diecisiete}</p>
+                <p className="palabras-dieciocho">{language.words.dieciocho}</p>
+                <p className="palabras-diecinueve">{language.words.diecinueve}</p>
+                <p className="palabras-veinte">{language.words.veinte}</p>
+            </div>
         </section>
     )
 }
